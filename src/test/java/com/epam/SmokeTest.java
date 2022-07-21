06db3d03a48f28f0033571ea012817b045199649
Sabitha Chinnam <sabitha_chinnam@epam.com>
2022-07-21 21:33:04 +0530

    Initial commit

---
+++ src/test/java/com/epam/SmokeTest.java
@@ -0,0 +1,12 @@
+package com.epam;
+
+import org.junit.jupiter.api.Test;
+
+import java.net.SocketOption;
+
+public class SmokeTest {
+    @Test
+    public void test(){
+        System.out.println("happy this is my first test case");
+}
+}
